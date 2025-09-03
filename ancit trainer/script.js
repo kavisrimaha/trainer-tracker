3a7c029d1e5085f1278e940dc7266f3ebaa88425
KaviSriMaha <kavisri4812@gmail.com>
2025-09-03 17:14:10 +0530

    update spoke

--- ancit trainer/script.js
+++ ancit trainer/script.js
@@ -1,95 +1,427 @@
-// Data structure for hubs and spokes
 const locationData = {
-      'Zone XII : Ariyalur': {
-        'Hub 1 (SRM TRP ENGINEERING COLLEGE)': {
-          colleges: ['SRM TRP ENGINEERING COLLEGE'],
-          spokes: { 'Spoke 1': [] }
-        },
-        'Hub 2 (ROEVER ENGINEERING COLLEGE)': {
-          colleges: ['ROEVER ENGINEERING COLLEGE'],
-          spokes: {
-            'Spoke 2': [
-              'DR.NAVALAR NEDUNCHEZHIYAN COLLEGE OF ENGINEERING',
-              'SRI RAMAKRISHNA COLLEGE OF ENGINEERING'
-            ]
-          }
-        },
-        'Hub 3 (UNIVERSITY COLLEGE OF ENGINEERING, ARIYALUR)': {
-          colleges: ['UNIVERSITY COLLEGE OF ENGINEERING, ARIYALUR'],
-          spokes: {
-            'Spoke 3': [
-              'MEENAKSHI RAMASWAMY ENGINEERING COLLEGE',
-              'NELLIANDAVAR INSTITUTE OF TECHNOLOGY'
-            ]
-          }
-        },
-        'Hub 4 (M.A.M College of Engineering)': {
-          colleges: ['M.A.M College of Engineering'],
-          spokes: {
-            'Spoke 4': ['M.A.M College of Engineering']
-          }
-        },
-        'Hub 5 (M.A.M. COLLEGE OF ENGINEERING AND TECHNOLOGY)': {
-          colleges: ['M.A.M. COLLEGE OF ENGINEERING AND TECHNOLOGY'],
-          spokes: {
-            'Spoke 5': ['TRICHY ENGINEERING COLLEGE']
-          }
-        }
+  "Zone XII : Ariyalur": {
+    "Hub 1 (SRM TRP ENGINEERING COLLEGE)": {
+      colleges: ["SRM TRP ENGINEERING COLLEGE"],
+      spokes: {
+        "Spoke 1": []
       }
-    };
-  const zoneSelect = document.getElementById('zone_name');
-    const hubSelect = document.getElementById('hub_name');
-    const spokeSelect = document.getElementById('spoke_name');
-
-    // When zone changes, populate hubs
-    zoneSelect.addEventListener('change', function() {
-      hubSelect.innerHTML = '<option value="">-- Select Hub --</option>';
-      spokeSelect.innerHTML = '<option value="">-- Select Spoke/College --</option>';
-
-      const hubs = locationData[this.value];
-      if (hubs) {
-        Object.keys(hubs).forEach(hub => {
-          const opt = document.createElement('option');
-          opt.value = hub;
-          opt.textContent = hub;
-          hubSelect.appendChild(opt);
-        });
+    },
+    "Hub 2 (ROEVER ENGINEERING COLLEGE)": {
+      colleges: ["ROEVER ENGINEERING COLLEGE"],
+      spokes: {
+        "Spoke 2": [
+          "DR.NAVALAR NEDUNCHEZHIYAN COLLEGE OF ENGINEERING",
+          "SRI RAMAKRISHNA COLLEGE OF ENGINEERING"
+        ]
       }
+    },
+    "Hub 3 (UNIVERSITY COLLEGE OF ENGINEERING, ARIYALUR)": {
+      colleges: ["UNIVERSITY COLLEGE OF ENGINEERING, ARIYALUR"],
+      spokes: {
+        "Spoke 3": [
+          "MEENAKSHI RAMASWAMY ENGINEERING COLLEGE",
+          "NELLIANDAVAR INSTITUTE OF TECHNOLOGY"
+        ]
+      }
+    },
+    "Hub 4 (M.A.M College of Engineering)": {
+      colleges: ["M.A.M College of Engineering"],
+      spokes: {
+        "Spoke 4": []
+      }
+    },
+    "Hub 5 (M.A.M. COLLEGE OF ENGINEERING AND TECHNOLOGY)": {
+      colleges: ["M.A.M. COLLEGE OF ENGINEERING AND TECHNOLOGY"],
+      spokes: {
+        "Spoke 5": ["TRICHY ENGINEERING COLLEGE"]
+      }
+    }
+  },
+  "Zone XIII : Pattukkottai": {
+    "Hub 6 (University College of Engineering Thirukkuvalai)": {
+      colleges: ["University College of Engineering Thirukkuvalai"],
+      spokes: {
+        "Spoke 6": ["ARIFA INSTITUTE OF TECHNOLOGY"]
+      }
+    },
+    "Hub 7 (ARASU ENGINEERING COLLEGE)": {
+      colleges: ["ARASU ENGINEERING COLLEGE"],
+      spokes: {
+        "Spoke 7": []
+      }
+    },
+    "Hub 8 (ANNAI COLLEGE OF ENGINEERING AND TECHNOLOGY)": {
+      colleges: ["ANNAI COLLEGE OF ENGINEERING AND TECHNOLOGY"],
+      spokes: {
+        "Spoke 8": []
+      }
+    },
+    "Hub 9 (K.S.K COLLEGE OF ENGINEERING AND TECHNOLOGY)": {
+      colleges: ["K.S.K COLLEGE OF ENGINEERING AND TECHNOLOGY"],
+      spokes: {
+        "Spoke 9": []
+      }
+    },
+    "Hub 10 (University College of Engineering Pattukkottai)": {
+      colleges: ["University College of Engineering Pattukkottai"],
+      spokes: {
+        "Spoke 10": [
+          "A.R.J. COLLEGE OF ENGINEERING AND TECHNOLOGY"
+        ]
+      }
+    },
+    "Hub 11 (GOVERNMENT COLLEGE OF ENGINEERING, THANJAVUR)": {
+      colleges: ["GOVERNMENT COLLEGE OF ENGINEERING, THANJAVUR"],
+      spokes: {
+        "Spoke 11": []
+      }
+    },
+    "Hub 12 (ANJALAI AMMAL MAHALINGAM ENGINEERING COLLEGE)": {
+      colleges: ["ANJALAI AMMAL MAHALINGAM ENGINEERING COLLEGE"],
+      spokes: {
+        "Spoke 12": ["VANDAYAR ENGINEERING COLLEGE"]
+      }
+    },
+    "Hub 13 (St.Joseph's College of Engineering and Technology , Thanjavur)": {
+      colleges: ["St.Joseph's College of Engineering and Technology , Thanjavur"],
+      spokes: {
+        "Spoke 13": [
+          "P.R.ENGINEERING COLLEGE",
+          "Star Lion College of Engineering and Technology"
+        ]
+      }
+    },
+    "Hub 14 (A.V.C COLLEGE OF ENGINEERING)": {
+      colleges: ["A.V.C COLLEGE OF ENGINEERING"],
+      spokes: {
+        "Spoke 14": ["AS-SALAM COLLEGE OF ENGINEERING and TECHNOLOGY"]
+      }
+    }
+  },
+  "Zone XV : Dindigul": {
+    "Hub 15 (UNIVERSITY COLLEGE OF ENGINEERING, DINDIGUL)": {
+      colleges: ["UNIVERSITY COLLEGE OF ENGINEERING, DINDIGUL"],
+      spokes: {
+        "Spoke 15": []
+      }
+    },
+    "Hub 16 (R.V.S. College of Engineering)": {
+      colleges: ["R.V.S. College of Engineering"],
+      spokes: {
+        "Spoke 16": [
+          "RVS EDUCATIONAL TRUSTS GROUP OF INSTITUTIONS",
+          "SBM COLLEGE OF ENGINEERING AND TECHNOLOGY"
+        ]
+      }
+    },
+    "Hub 17 (N.S.N. College of Engineering and Technology)": {
+      colleges: ["N.S.N. College of Engineering and Technology"],
+      spokes: {
+        "Spoke 17": []
+      }
+    },
+    "Hub 18 (CHETTINAD COLLEGE OF ENGINEERING AND TECHNOLOGY)": {
+      colleges: ["CHETTINAD COLLEGE OF ENGINEERING AND TECHNOLOGY"],
+      spokes: {
+        "Spoke 18": ["Arulmurugan College Of Engineering, Karur"]
+      }
+    },
+    "Hub 19 (GOVERNMENT COLLEGE OF ENGINEERING - Theni)": {
+      colleges: ["GOVERNMENT COLLEGE OF ENGINEERING - Theni"],
+      spokes: {
+        "Spoke 19": ["Theni Kammavar Sangam College of Technology"]
+      }
+    },
+    "Hub 20 (Nadar Saraswathi College of Engineering and Technology)": {
+      colleges: ["Nadar Saraswathi College of Engineering and Technology"],
+      spokes: {
+        "Spoke 20": ["BHARATH NIKETAN ENGINEERING COLLEGE"]
+      }
+    },
+    "Hub 21 (CHRISTIAN COLLEGE OF ENGINEERING AND TECHNOLOGY)": {
+      colleges: ["CHRISTIAN COLLEGE OF ENGINEERING AND TECHNOLOGY"],
+      spokes: {
+        "Spoke 21": []
+      }
+    }
+  },
+  "Zone XVI : Tirunelveli-I": {
+    "Hub 22 (Government College of Engineering)": {
+      colleges: ["Government College of Engineering"],
+      spokes: {
+        "Spoke 22": []
+      }
+    },
+    "Hub 23 (PSN INSTITUTE OF TECHNOLOGY AND SCIENCE)": {
+      colleges: ["PSN INSTITUTE OF TECHNOLOGY AND SCIENCE"],
+      spokes: {
+        "Spoke 23": ["SCAD College of Engineering and Technology"]
+      }
+    },
+    "Hub 24 (P.S.R.R COLLEGE OF ENGINEERING)": {
+      colleges: ["P.S.R.R COLLEGE OF ENGINEERING"],
+      spokes: {
+        "Spoke 24": [
+          "Renganayagi Varatharaj college of engineering",
+          "Unnamalai Institute of Technology"
+        ]
+      }
+    },
+    "Hub 25 (EINSTEIN COLLEGE OF ENGINEERING)": {
+      colleges: ["EINSTEIN COLLEGE OF ENGINEERING"],
+      spokes: {
+        "Spoke 25": ["PSN ENGINEERING COLLEGE"]
+      }
+    },
+    "Hub 26 (J P COLLEGE OF ENGINEERING)": {
+      colleges: ["J P COLLEGE OF ENGINEERING"],
+      spokes: {
+        "Spoke 26": ["Sardar Raja College of Engineering"]
+      }
+    },
+    "Hub 27 (S.VEERASAMY CHETTIAR COLLEGE OF ENGINEERING AND TECHNOLOGY)": {
+      colleges: ["S.VEERASAMY CHETTIAR COLLEGE OF ENGINEERING AND TECHNOLOGY"],
+      spokes: {
+        "Spoke 27": ["MAHAKAVI BHARATHIYAR COLLEGE OF ENGINEERING AND TECHNOLOGY"]
+      }
+    }
+  },
+  "Zone XVII : Nagercoil": {
+    "Hub 28 (UNIVERSITY COLLEGE OF ENGINEERING, NAGERCOIL)": {
+      colleges: ["UNIVERSITY COLLEGE OF ENGINEERING, NAGERCOIL"],
+      spokes: {
+        "Spoke 28": ["Vins Christian College of engineering"]
+      }
+    },
+    "Hub 29 (PONJESLY COLLEGE OF ENGINEERING)": {
+      colleges: ["PONJESLY COLLEGE OF ENGINEERING"],
+      spokes: {
+        "Spoke 29": ["MARTHANDAM COLLEGE OF ENGINEERING AND TECHNOLOGY"]
+      }
+    },
+    "Hub 30 (ARUNACHALA COLLEGE OF ENGINEERING FOR WOMEN)": {
+      colleges: ["ARUNACHALA COLLEGE OF ENGINEERING FOR WOMEN"],
+      spokes: {
+        "Spoke 30": []
+      }
+    },
+    "Hub 31 (MAR EPHRAEM COLLEGE OF ENGINEERING AND TECHNOLOGY)": {
+      colleges: ["MAR EPHRAEM COLLEGE OF ENGINEERING AND TECHNOLOGY"],
+      spokes: {
+        "Spoke 31": [
+          "NARAYANAGURU COLLEGE OF ENGINEERING",
+          "ARUNACHALA HITECH ENGINEERING COLLEGE",
+          "GOOD SHEPHERD COLLEGE OF ENGINEERING and TECHNOLOGY",
+          "MARIA COLLEGE OF ENGINEERING AND TECHNOLOGY"
+        ]
+      }
+    },
+    "Hub 32 (UDAYA SCHOOL OF ENGINEERING)": {
+      colleges: ["UDAYA SCHOOL OF ENGINEERING"],
+      spokes: {
+        "Spoke 32": [
+          "BETHLAHEM INSTITUTE OF ENGINEERING",
+          "IMMANUEL ARASAR J J COLLEGE OF ENGINEERING",
+          "LORD JEGANNATH COLLEGE OF ENGINEERING AND TECHNOLOGY"
+        ]
+      }
+    },
+    "Hub 33 (Loyola Institute of Technology and Science)": {
+      colleges: ["Loyola Institute of Technology and Technology"],
+      spokes: {
+        "Spoke 33": [
+          "ANNAI VAILANKANNI COLLEGE OF ENGINEERING, KANYAKUMARI",
+          "C.S.I. INSTITUTE OF TECHNOLOGY"
+        ]
+      }
+    }
+  },
+  "Zone XVIII : Trichy": {
+    "Hub 35 (University College of Engineering, BIT Campus, Trichy)": {
+      colleges: ["University College of Engineering, BIT Campus, Trichy"],
+      spokes: {
+        "Spoke 35": []
+      }
+    },
+    "Hub 36 (Pavendar Bharathidasan College of Engineering and Technology)": {
+      colleges: ["Pavendar Bharathidasan College of Engineering and Technology"],
+      spokes: {
+        "Spoke 36": [
+          "MOOKAMBIGAI COLLEGE OF ENGINEERING",
+          "Sudharsan Engineering College,Pudukkottai"
+        ]
+      }
+    },
+    "Hub 37 (SHIVANI ENGINEERING COLLEGE)": {
+      colleges: ["SHIVANI ENGINEERING COLLEGE"],
+      spokes: {
+        "Spoke 37": ["OXFORD ENGINEERING COLLEGE"]
+      }
+    },
+    "Hub 38 (IMAYAM COLLEGE OF ENGINEERING)": {
+      colleges: ["IMAYAM COLLEGE OF ENGINEERING"],
+      spokes: {
+        "Spoke 38": [
+          "JAYARAM COLLEGE OF ENGINEERING AND TECHNOLOGY",
+          "OASYS Institute of Technology, Trichy",
+          "Sri Vidya college of engineering and technology"
+        ]
+      }
+    },
+    "Hub 39 (Government College of Engineering Srirangam)": {
+      colleges: ["Government College of Engineering Srirangam"],
+      spokes: {
+        "Spoke 39": []
+      }
+    }
+  },
+  "Zone XX : Tirunelveli-II": {
+    "Hub 40 (Jayaraj Annapackiam CSI College of Engineering)": {
+      colleges: ["Jayaraj Annapackiam CSI College of Engineering"],
+      spokes: {
+        "Spoke 40": []
+      }
+    },
+    "Hub 41 (Holycross Engineering college)": {
+      colleges: ["Holycross Engineering college"],
+      spokes: {
+        "Spoke 41": []
+      }
+    },
+    "Hub 42 (M.E.T. ENGINEERING COLLEGE)": {
+      colleges: ["M.E.T. ENGINEERING COLLEGE"],
+      spokes: {
+        "Spoke 42": [
+          "CAPE INSTITUTE OF TECHNOLOGY",
+          "DMI ENGINEERING COLLEGE",
+          "Jayamatha Engineering College",
+          "SATYAM COLLEGE OF ENGINEERING AND TECHNOLOGY"
+        ]
+      }
+    },
+    "Hub 43 (ANNA UNIVERSITY REGIONAL CAMPUS, TIRUNELVELI)": {
+      colleges: ["ANNA UNIVERSITY REGIONAL CAMPUS, TIRUNELVELI"],
+      spokes: {
+        "Spoke 43": []
+      }
+    },
+    "Hub 44 (V V College of Engineering)": {
+      colleges: ["V V College of Engineering"],
+      spokes: {
+        "Spoke 44": []
+      }
+    },
+    "Hub 45 (Dr.G.U.Pope College of Engineering)": {
+      colleges: ["Dr.G.U.Pope College of Engineering"],
+      spokes: {
+        "Spoke 45": ["ST.MOTHER THERESA ENGINEERING COLLEGE"]
+      }
+    },
+    "Hub 46 (PET ENGINEERING COLLEGE, VALLIOOR)": {
+      colleges: ["PET ENGINEERING COLLEGE, VALLIOOR"],
+      spokes: {
+        "Spoke 46": ["Universal College of Engineering and Technology Vallioor"]
+      }
+    },
+    "Hub 47 (UNIVERSITY COLLEGE OF ENGINEERING, THOOTHUKUDI)": {
+      colleges: ["UNIVERSITY COLLEGE OF ENGINEERING, THOOTHUKUDI"],
+      spokes: {
+        "Spoke 47": [
+          "GRACE COLLEGE OF ENGINEERING",
+          "Dr.Sivanthi Aditanar College of Engineering, Tiruchendur",
+          "INFANT JESUS COLLEGE OF ENGINEERING",
+          "ST.MOTHER THERESA ENGINEERING COLLEGE"
+        ]
+      }
+    }
+  },
+  "Zone XXII : Madurai": {
+    "Hub 48 (Anna University Regional Campus Madurai)": {
+      colleges: ["Anna University Regional Campus Madurai"],
+      spokes: {
+        "Spoke 48": []
+      }
+    },
+    "Hub 49 (MANGAYARKARASI COLLEGE OF ENGINEERING)": {
+      colleges: ["MANGAYARKARASI COLLEGE OF ENGINEERING"],
+      spokes: {
+        "Spoke 49": ["P.T.R.COLLEGE OF ENGINEERING AND TECHNOLOGY"]
+      }
+    },
+    "Hub 50 (LATHA MATHAVAN ENGINEERING COLLEGE)": {
+      colleges: ["LATHA MATHAVAN ENGINEERING COLLEGE"],
+      spokes: {
+        "Spoke 50": ["ULTRA College of Engineeringand Technology"]
+      }
+    },
+    "Hub 51 (SACS M.A.V.M.M ENGINEERING COLLEGE)": {
+      colleges: ["SACS M.A.V.M.M ENGINEERING COLLEGE"],
+      spokes: {
+        "Spoke 51": ["Vaigai College of Engineering"]
+      }
+    }
+  }
+};
+
+const zoneSelect = document.getElementById('zone_name');
+const hubSelect = document.getElementById('hub_name');
+const spokeSelect = document.getElementById('spoke_name');
+
+// Function to populate the zone dropdown
+function populateZones() {
+  zoneSelect.innerHTML = '<option value="">-- Select Zone --</option>';
+  Object.keys(locationData).forEach(zone => {
+    const opt = document.createElement('option');
+    opt.value = zone;
+    opt.textContent = zone;
+    zoneSelect.appendChild(opt);
+  });
+}
+
+// Populate zones on page load
+populateZones();
+
+// When zone changes, populate hubs
+zoneSelect.addEventListener('change', function() {
+  hubSelect.innerHTML = '<option value="">-- Select Hub --</option>';
+  spokeSelect.innerHTML = '<option value="">-- Select Spoke/College --</option>';
+
+  const hubs = locationData[this.value];
+  if (hubs) {
+    Object.keys(hubs).forEach(hub => {
+      const opt = document.createElement('option');
+      opt.value = hub;
+      opt.textContent = hub;
+      hubSelect.appendChild(opt);
     });
+  }
+});
 
-    // When hub changes, populate spokes and colleges
-    hubSelect.addEventListener('change', function() {
-      spokeSelect.innerHTML = '<option value="">-- Select Spoke/College --</option>';
+// When hub changes, populate spokes and colleges
+hubSelect.addEventListener('change', function() {
+  spokeSelect.innerHTML = '<option value="">-- Select Spoke/College --</option>';
 
-      const zone = zoneSelect.value;
-      const hub = hubSelect.value;
-      const hubData = locationData[zone][hub];
+  const zone = zoneSelect.value;
+  const hub = hubSelect.value;
+  const hubData = locationData[zone][hub];
 
-      if (hubData) {
-        // Add hub colleges
-        hubData.colleges.forEach(college => {
+  if (hubData) {
+    // Add spokes + their colleges (skip the hub colleges entirely)
+    Object.keys(hubData.spokes).forEach(spoke => {
+      const spokeColleges = hubData.spokes[spoke];
+      if (spokeColleges.length > 0) {
+        spokeColleges.forEach(college => {
           const opt = document.createElement('option');
           opt.value = college;
-          opt.textContent = `Hub College: ${college}`;
+        opt.textContent = `${spoke} - ${college}`; // Just the college name
           spokeSelect.appendChild(opt);
         });
-
-        // Add spokes + their colleges
-        Object.keys(hubData.spokes).forEach(spoke => {
-          const spokeColleges = hubData.spokes[spoke];
-          if (spokeColleges.length > 0) {
-            spokeColleges.forEach(college => {
-              const opt = document.createElement('option');
-              opt.value = college;
-              opt.textContent = `${spoke} - ${college}`;
-              spokeSelect.appendChild(opt);
-            });
-          } else {
-            const opt = document.createElement('option');
-            opt.value = spoke;
-            opt.textContent = `${spoke} (no colleges)`;
-            spokeSelect.appendChild(opt);
-          }
-        });
+      } else {
+        const opt = document.createElement('option');
+        opt.value = spoke;
+        opt.textContent = `${spoke} (no colleges)`;
+        spokeSelect.appendChild(opt);
       }
     });
+  }
+});
